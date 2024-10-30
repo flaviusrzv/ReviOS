8319bbfdb5c7d31242ddbea133b47db874b025a3
GitHub Action <action@github.com>
2024-10-30 01:59:29 +0000

    Updated supported versions and readme

--- src/Executables/FINALIZE.cmd
+++ src/Executables/FINALIZE.cmd
@@ -1,5 +1,5 @@
 @echo off
-set version=24.10.29
+set version=24.10.30
 for /f "tokens=3" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "CurrentBuild"') do set "build=%%i"
 if %build% gtr 19045 ( set "w11=true" )
 
